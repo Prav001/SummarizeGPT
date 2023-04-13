d75085e552ae2a0447e5f1b0b68c582db5b1db32
Praveen Sharma <praveensharma900@gmail.com>
2023-04-13 16:49:54 -0400

    changes for prod deployment

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def generate_summary():
 
     return jsonify(content=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)       
+# if __name__ == '__main__':
+#     app.run(debug=True)       
